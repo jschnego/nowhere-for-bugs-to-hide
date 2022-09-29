9ff28b2a365cf29bea77b7cb4d6e428be641bc18
Jan Schnegotzki <jan.schnegotzki@zuhlke.com>
2022-09-29 13:41:32 +0800

    Refactor to template-based approach

--- src/main/java/LikesFormatter.java
+++ src/main/java/LikesFormatter.java
@@ -2,19 +2,22 @@ import java.util.List;
 
 public class LikesFormatter {
     public static String format(List<String> users) {
-        String name = users.isEmpty() ? "No one" : concatenate(users);
         String suffix = users.size() > 1 ? "" : "s";
-        return String.format("%s like%s this", name, suffix);
+        return String.format("%s like%s this", concatenate(users), suffix);
     }
 
     private static String concatenate(List<String> users) {
-        if (users.size() > 3) {
-            return String.format("%s, %s and %s others", users.get(0), users.get(1), users.size() - 2);
+        switch (users.size()) {
+            case 0:
+                return "No one";
+            case 1:
+                return users.get(0);
+            case 2:
+                return String.format("%s and %s", users.get(0), users.get(1));
+            case 3:
+                return String.format("%s, %s and %s", users.get(0), users.get(1), users.get(2));
+            default:
+                return String.format("%s, %s and %s others", users.get(0), users.get(1), users.size() - 2);
         }
-        String joined = String.join(" and ", users);
-        if (users.size() == 3) {
-            joined = joined.replaceFirst(" and ", ", ");
-        }
-        return joined;
     }
 }
