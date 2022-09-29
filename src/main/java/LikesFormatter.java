c59cddfcc0e5baf364bc1a8647356fb4d27bc48e
Jan Schnegotzki <jan.schnegotzki@zuhlke.com>
2022-09-29 11:56:21 +0800

    Implement the one user case

--- src/main/java/LikesFormatter.java
+++ src/main/java/LikesFormatter.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class LikesFormatter {
     public static String format(List<String> users) {
-        return "No one likes this";
+        String name = users.isEmpty() ? "No one" : users.get(0);
+        return String.format("%s likes this", name);
     }
 }
--- src/test/java/LikesFormatterTest.java
+++ src/test/java/LikesFormatterTest.java
@@ -17,5 +17,7 @@ public class LikesFormatterTest {
     @Test
     @DisplayName("For a single user, their name should be indicated")
     void shouldGiveTheNameForSingleUser() {
+        String label = LikesFormatter.format(List.of("Jane"));
+        assertThat(label).isEqualTo("Jane likes this");
     }
 }
